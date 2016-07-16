e9799c5309312e26654a4194c06fe1c1d9f7b54b
greatontime <pathomphong1144@gmail.com>
2016-07-17 00:48:47 +0700

    update controller

--- routers/user.go
+++ routers/user.go
@@ -1,12 +1,12 @@
 package routers
 
 import (
-	"controllers"
+	"github.com/greatontime/controller"
 	"github.com/gorilla/mux"
 )
 
 func SetUserRoutes(router *mux.Router) *mux.Router {
-	router.HandleFunc("/users/register", controllers.Register).Methods("POST")
-	router.HandleFunc("/users/login", controllers.Login).Methods("POST")
+	router.HandleFunc("/users/register", controller.Register).Methods("POST")
+	router.HandleFunc("/users/login", controller.Login).Methods("POST")
 	return router
 }
